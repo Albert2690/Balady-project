8845944be0d480ccea2dec559483231e6e4a5303
Anisha <anishaanuu308@gmail.com>
2024-07-27 20:42:05 +0530

    updated tailwind css config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Layout from "./Layout/layout.jsx"
 
 function App() {
 
   return (
     <>
-      </>
-  )
+     <Layout/>
+    </>
+  );
 }
 
 export default App
