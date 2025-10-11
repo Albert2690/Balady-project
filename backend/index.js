0a5fd77fbbc3e078f5502e2a558a7dda6f6ee0c9
Albert2690 <albersebastian2525@gmail.com>
2025-10-11 17:12:19 +0530

    commit on october

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
   origin: [
-    process.env.CLIENT_URL || 'http://www.balady.org.in',
+    process.env.CLIENT_URL || 'http://www.balady.website',
     
   ],
   
